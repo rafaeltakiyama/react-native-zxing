d7a218ee0bc4d78dac2dc8d3bf7bd1eda0c28461
Rafael Akiyama <rafael.akiyama@luizalabs.com>
2018-11-21 23:25:57 -0200

    remove unnecessary files

---
+++ rnzxing.js
@@ -0,0 +1,12 @@
+/**
+ * @providesModule react-native-device-info
+ */
+import { NativeModules } from 'react-native'
+
+const ScannerModule = NativeModules.ScannerModule
+
+export default {
+  openScanner: function () {
+    return ScannerModule.openScanner
+  }
+}
