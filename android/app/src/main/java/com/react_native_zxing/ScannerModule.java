61d1bf3d91b1b3da835262cb959824088d1744df
Francois Nadeau <fnadeau@nu-book.com>
2019-12-12 11:48:03 -0500

    Changed lib to use AndroidX
    import change to support AndroidX.

--- android/app/src/main/java/com/react_native_zxing/ScannerModule.java
+++ android/app/src/main/java/com/react_native_zxing/ScannerModule.java
@@ -2,7 +2,7 @@ package com.react_native_zxing;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
